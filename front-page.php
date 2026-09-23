4929fd97484d0067756c4f557eb80c128d9b959c
staylor97 <s.taylor889@yahoo.co.uk>
2026-09-23 16:42:58 +0100

    refactor: prepare front page for custom ACF fields

--- front-page.php
+++ front-page.php
@@ -1,13 +1,47 @@
 <?php
-    /**
-     * WebsiteNI Joints - Front Page Template
-     * WebsiteNI Starter Theme built on JointsWP
-     * Created by WebsiteNI.
-     */
-    
-    get_header();
-    ?> 
-<main id="content" class="">
+/**
+ * WebsiteNI Starter Theme
+ *
+ * Front page template.
+ *
+ * Homepage content is built using dedicated
+ * ACF field groups rather than Flexible Content.
+ */
+
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+<main
+	id="content"
+	class="content"
+>
+
+	<?php if (have_posts()) : ?>
+
+		<?php
+		while (have_posts()) :
+			the_post();
+			?>
+
+			<?php
+			/**
+			 * Homepage sections are added here
+			 * on a project-by-project basis.
+			 *
+			 * Example:
+			 *
+			 * $hero_title = get_field('hero_title');
+			 * $hero_image = get_field('hero_image');
+			 */
+			?>
+
+		<?php endwhile; ?>
+
+	<?php endif; ?>
 
 </main>
-<?php get_footer(); ?>
+
+<?php
+get_footer();
