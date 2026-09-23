0d6b4d26efec5a61ba289cea66caa26959b3db61
staylor97 <s.taylor889@yahoo.co.uk>
2026-09-23 15:35:37 +0100

    refactor: modernise 404 template

--- 404.php
+++ 404.php
@@ -1,26 +1,55 @@
 <?php
 /**
-* websiteni Joints
-* websiteni Starter Theme built on JointsWP; http://jointswp.com/.
-* Created by websiteni.
-*/
-
+ * WebsiteNI Starter Theme
+ *
+ * 404 template.
+ */
+defined('ABSPATH') || exit;
 get_header();
 ?>
-<main id="content">
-    <div class="grid-container full">
-        <div class="grid-container">
-            <div class="grid-x grid-padding-x">
-                <div class="cell text-center">
-                    <h1>Oops! Page not found.</h2>
-                    <p>
-                        Sorry, the page you are looking for doesn’t exist or has been moved.
-                    </p>
-                    <a href="<?php echo esc_url(home_url('/')); ?>" class="button">Return Home</a>
-                </div>
-
-            </div>
-        </div>
-    </div>
+<main
+	id="content"
+	class="content"
+>
+	<section class="error-404">
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="cell small-12">
+					<div class="error-404-content text-center">
+						<h1 class="error-404-title">
+							<?php
+							esc_html_e(
+								'Oops! Page not found.',
+								'websiteni-foundation'
+							);
+							?>
+						</h1>
+						<p class="error-404-text">
+							<?php
+							esc_html_e(
+								'Sorry, the page you are looking for doesn’t exist or may have been moved.',
+								'websiteni-foundation'
+							);
+							?>
+						</p>
+						<a
+							class="button error-404-button"
+							href="<?php echo esc_url(
+								home_url('/')
+							); ?>"
+						>
+							<?php
+							esc_html_e(
+								'Return Home',
+								'websiteni-foundation'
+							);
+							?>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
-<?php get_footer(); ?>
+<?php
+get_footer();
